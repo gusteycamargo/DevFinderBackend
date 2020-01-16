f40b7d268ab72bed259dec3eab82803a5388f483
gustaacamargo <camargo25.gustavo@gmail.com>
2020-01-16 09:56:02 -0300

    Primeira versão do backend finalizada

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,27 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(request, response) {
+        const {latitude, longitude, techs } = request.query;
+        const techsArray = parseStringAsArray(techs);
+
+        console.log(techsArray);
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                },
+            },
+        });
+
+        return response.json(devs);
+    }
+}
